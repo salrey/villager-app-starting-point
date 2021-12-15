89e22a2ee3c6af7341060877bc4d5ba2ff2929bd
salrey <tekdehn@gmail.com>
2021-12-15 11:50:44 -0500

    Setup state, methods

--- src/App.js
+++ src/App.js
@@ -7,21 +7,47 @@ class App extends Component {
     super();
 
     this.state = {
+      // no need to use data as a state for now, but this one will change when we use an API
       villagerData: villagerData,
-      villagers: [],
+      villagers: [villagerData.ant00],
+      currentVillager: villagerData.ant00
     };
   }
 
+  handleChange = () => {
+    // const { villagerData } = this.state
+
+    // store it's length and do random 
+    // Consider Object.values / Object.entries
+
+    this.setState({currentVillager: "some random villager obj"})
+  }
+
+  handleAdd = () => {
+    const { villagers, currentVillager } = this.state
+
+    this.setState({villagers: [...villagers, currentVillager]})
+  }
+
+  handleClear = () => {
+    this.setState({
+      villagers: [villagerData.ant00],
+      currentVillager: villagerData.ant00
+    })
+  }
+
   render() {
+    const { villagers, currentVillager } = this.state
+
     return (
       <div className="App">
         <h1>Animal Crossing Villagers</h1>
         <div>
-          <button>Change villager</button>
-          <button>Add villager to village</button>
-          <button>Clear Village</button>
-          <div>Currently selected villager:</div>
-          <div>Number of villagers in village: </div>
+          <button onClick={this.handleChange}>Change Villager</button>
+          <button onClick={this.handleAdd}>Add Villager to Village</button>
+          <button onClick={this.handleClear}>Clear Village</button>
+          <div>Current Villager: {currentVillager.name["name-USen"]}</div>
+          <div>Total Villagers in Village: {villagers.length}</div>
           <div className="villager-grid"></div>
         </div>
       </div>
